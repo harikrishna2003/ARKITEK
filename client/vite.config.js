822c64c216e2d052f783585b5e436946dc688078
Harikrishna Pillai <115789694+harikrishna2003@users.noreply.github.com>
2025-07-27 10:55:47 +0530

    Add files via upload

---
+++ client/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+
+export default defineConfig({
+  // base: './', // 🔧 critical for Electron builds!
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+    port: 5173,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+})
